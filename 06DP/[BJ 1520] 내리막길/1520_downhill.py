6642ccc881e141777fdce45e3043727dfbdf276a
Jihae Na <njnjh0317@gmail.com>
2020-09-30 14:29:13 +0900

    백준 1520 다시 풀기 완료

--- 06DP/[BJ 1520] 내리막길/1520_downhill.py
+++ 06DP/[BJ 1520] 내리막길/1520_downhill.py
@@ -4,31 +4,32 @@ dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
 M, N = map(int, input().split())
 flag = [[-1]*N for _ in range(M)]
-num = 0
-def find_downhill(road, x, y):
+def find_route(x, y, road):
     if(x == N-1 and y == M-1):
         return 1
-    if(flag[y][x]!=-1): #방문한 적이 있다면
+    if(flag[y][x]!=-1):
         return flag[y][x]
-    flag[y][x] = 0 #방문 표시
-    for i in range(0, 4):
-        nextX = x + dx[i]
-        nextY = y + dy[i]
-
-        if(nextX>=0 and nextY>=0 and nextX<N and nextY < M):
-            if(road[nextY][nextX]<road[y][x]):
-                flag[y][x] += find_downhill(road, nextX, nextY)
-
-    return flag[y][x]
     
+    flag[y][x]=0
+
+    for i in range(4):
+        nextx = x+dx[i]
+        nexty = y+dy[i]
+
+        if(nextx>=0 and nexty>=0 and nextx<N and nexty<M):
+            if(road[nexty][nextx]<road[y][x]):
+                flag[y][x]+=find_route(nextx, nexty, road)
+        
+    return flag[y][x]
 
 
 road = []
 for i in range(M):
     road.append(list(map(int, input().split())))
 
-
-find_downhill(road, 0, 0)
+find_route(0, 0, road)
 print(flag[0][0])
+
+
 
 
