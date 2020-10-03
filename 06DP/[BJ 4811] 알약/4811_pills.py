1447b37698b6adc0398ce083ba5473a323a9ac38
Jihae Na <njnjh0317@gmail.com>
2020-10-04 01:59:34 +0900

    백준 4811 알약 성공

--- 06DP/[BJ 4811] 알약/4811_pills.py
+++ 06DP/[BJ 4811] 알약/4811_pills.py
@@ -1,3 +1,14 @@
 import sys
 if __name__ == "__main__":
-    
+    while True:
+        N = int(sys.stdin.readline())
+        if(N == 0):
+            break
+        number = [1]
+        for i in range(N):
+            num = 0
+            for j in range(i+1):
+                num+=number[i-j] * number[j]
+            number.append(num)
+        
+        print(number[N])
